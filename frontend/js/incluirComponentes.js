85eb17c54a50a3a1ec952eea0c77118b82b9f61a
ahlr <ahlr_2000@yahoo.com>
2025-05-20 18:04:59 -0300

    Implementação do cadastro de usuário

--- frontend/js/incluirComponentes.js
+++ frontend/js/incluirComponentes.js
@@ -11,8 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   incluir("cabecalho", "componentes/cabecalho.html");
-  incluir("senhaRegrasContainer", "componentes/senhaRegras.html");
+  
+  incluir("senhaRegrasContainer", "componentes/senhaRegras.html", () => {
+    // Aguarda DOM atualizar com os <li> carregados
+    requestAnimationFrame(() => {
+      import("./validacaoSenha.js")
+        .then(() => console.log("✅ validacaoSenha.js carregado"))
+        .catch(err => console.error("❌ Erro ao importar validacaoSenha.js:", err));
+    });
+  });
+  
+
   incluir("modalAvisoContainer", "componentes/modalAviso.html");
+  
   incluir("rodape", "componentes/rodape.html", () => {
     const hoje = new Date();
     const inicial = new Date(hoje.getFullYear(), 0, 1);
